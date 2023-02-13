71b72af381e5682b746df1c7c8e1936dbcb02737
Victor Garcia Fernandez <victorgf2011@gmail.com>
2023-02-13 16:42:10 +0100

    update

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace chemical_dam;
 
-partial class Form1
+partial class Main
 {
     /// <summary>
     ///  Required designer variable.
@@ -28,10 +28,17 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+            this.SuspendLayout();
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Main";
+            this.Text = "Chemical";
+            this.ResumeLayout(false);
+
     }
 
     #endregion
--- Main.cs
+++ Main.cs
@@ -1,8 +1,8 @@
 namespace chemical_dam;
 
-public partial class Form1 : Form
+public partial class Main : Form
 {
-    public Form1()
+    public Main()
     {
         InitializeComponent();
     }
