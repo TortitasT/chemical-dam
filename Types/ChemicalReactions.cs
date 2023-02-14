2e37b3f1616ed50c65c9d1436c98d6e0812dd567
bipo38 <bipoben7@gmail.com>
2023-02-14 21:10:57 +0100

    Chemical Reactions type

---
+++ Types/ChemicalReactions.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Numerics;
+
+namespace chemical_dam.Types
+{
+     enum Type
+    {
+        synthesis,
+        decomposition, 
+        displacement,
+        acid-base, 
+        redox, 
+        others
+
+    }
+
+    internal class ChemicalReactions
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string description { get; set; }
+        Type type { get; set; }
+        string energyRelationship { get; set; }
+
+        //Vector<ChemicalCompounds> chemicalCompunds { get; set; }
+
+
+        
+    }
+}
