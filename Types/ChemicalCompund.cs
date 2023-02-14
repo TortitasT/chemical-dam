375d96dc4eec5ed20effeac1c35ed92cea719058
PatolokoPato <patoloko152@gmail.com>
2023-02-14 20:55:07 +0100

    compound struct

---
+++ Types/ChemicalCompund.cs
@@ -0,0 +1,11 @@
+﻿namespace chemical_dam.Types
+{
+    internal struct ChemicalCompound
+    {
+        int CASNumber { get; set; }
+        string CompoundName { get; set; }
+        string[] ComposedBy { get; set; } = new string[7];
+        int[] ElementsCount { get; set; } = new int[7];
+    }
+}
+
